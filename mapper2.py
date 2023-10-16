160ea64785a001df38c7e618c6e3ac8de5c14838
AnilMandavilli594 <61509925+AnilMandavilli594@users.noreply.github.com>
2023-10-16 18:24:28 -0400

    Update mapper2.py

--- mapper2.py
+++ mapper2.py
@@ -1,10 +1,11 @@
-import sys
 import re
-search_hours = sys.argv[1]
-start, end = search_hours.split('-')
-pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).*?\d{4}:(?P<hour>\d{2}):\d{2}.*? ')
+import sys
+
+conditions = sys.argv[1]
+begin, close = conditions.split('-')
+
+pat = re.compile('(?P<ip>\d+.\d+.\d+.\d+).?\d{4}:(?P<hour>\d{2}):\d{2}.? ')
 for line in sys.stdin:
-    line = line.strip()
-    match = pat.search(line)
-    if match and (int(match.group('hour')) >= int(start) and int(match.group('hour')) < int(end)):
-        print('%s\t%s'% (match.group('hour')+ '\t' +match.group('ip'), 1))
+  match = pat.search(line)
+  if match and (int(match.group('hour')) >= int(begin) and int(match.group('hour')) < int(close)):
+    print('%s\t%s'% (match.group('hour')+ '\t' +match.group('ip'), 1))
